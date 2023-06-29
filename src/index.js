b92b36e83de66aa38429b928d167faf33c13a77f
ArshavineRoy <arshavinewaema@gmail.com>
2023-06-29 22:30:31 +0300

    film list buttons

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-fetch('https://arshavineroy.github.io/phase-1-week-3-code-challenge/db.json')
-    .then(res => res.json())
-    .then(data => {
-        const array = Object.values(data.films); 
-        films(array)
-        
-    })
+fetch("https://arshavineroy.github.io/phase-1-week-3-code-challenge/db.json")
+  .then((res) => res.json())
+  .then((data) => {
+    const array = Object.values(data.films);
+    films(array);
+  });
 
-// testing DB
+// film list buttons
 
-function films (array){
-    array.forEach(film => {
-        const title = film.title
-        console.log(title);        
-    });
+function films(array) {
+  const movieList = document.querySelector(".movie-list");
+  array.forEach((film) => {
+    const title = film.title;
+    console.log(title);
+    const movieButton = document.createElement("button");
+    movieList.appendChild(movieButton);
+    movieButton.classList = "button-55";
+    movieButton.textContent = title;
+  });
 }
-
-// working great!
