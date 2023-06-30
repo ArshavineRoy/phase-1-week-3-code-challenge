47d5a1201054fbe978e5dc198ed4d4bcfc334e6d
ArshavineRoy <arshavinewaema@gmail.com>
2023-06-30 15:47:39 +0300

    sidebar, poster, footer

--- src/index.js
+++ src/index.js
@@ -8,13 +8,14 @@ fetch("https://arshavineroy.github.io/phase-1-week-3-code-challenge/db.json")
 // film list buttons
 
 function films(array) {
-  const movieList = document.querySelector(".movie-list");
+  const filmDiv = document.createElement("div");
+  filmDiv.classList.add("movie-list");
   array.forEach((film) => {
     const title = film.title;
-    console.log(title);
     const movieButton = document.createElement("button");
-    movieList.appendChild(movieButton);
-    movieButton.classList = "button-55";
+    document.querySelector(".sidebar").appendChild(filmDiv);
+    filmDiv.appendChild(movieButton);
+    movieButton.classList.add("button-55");
     movieButton.textContent = title;
   });
 }
